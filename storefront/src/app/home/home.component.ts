66459032161ef718f6bb3e606a4b9f8cbe726d3c
renansouz <renanss2005@gmail.com>
2024-05-30 18:57:08 -0300

    fetching project form server

--- storefront/src/app/home/home.component.ts
+++ storefront/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { Products } from '../../types';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +11,13 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor(
+    private productsServive: ProductsService
+  ) { }
+
+  ngOnInit() {
+    this.productsServive.getProducts('http://localhost:3000/clothes', {page: 0, perPage: 5}).subscribe((products: Products) => {
+      console.log(products.items);
+    })
+  }
 }
